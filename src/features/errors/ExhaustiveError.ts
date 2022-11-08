f66ab00f3d7177547ddd91ddd340487be18380b1
keitakn <keita.koga.work@gmail.com>
2022-11-08 23:32:46 +0900

    :sparkles: 網羅性チェックを利用したErrorを定義

---
+++ src/features/errors/ExhaustiveError.ts
@@ -0,0 +1,6 @@
+export class ExhaustiveError extends Error {
+  // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+  constructor(value: never, message = `Unsupported type: ${value}`) {
+    super(message);
+  }
+}
